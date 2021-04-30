c2934c922418ebedbf8d94e54d3de403c08d2055
sanjeeth r <9209035+sanjeeth-07@users.noreply.github.com>
2021-04-30 14:28:45 +0530

    Update e2e_tests.js

--- e2e_tests.js
+++ e2e_tests.js
@@ -1,6 +1,7 @@
 // <reference types="cypress" />
 
-describe("OpenOceanStudio:Crew Applications ", () => {
+describe("OpenOceanStudio:Crew Applications - E2E TEST CASES ", () => {
+  
   it("search for the candidates from homepage", () => {
     // visit the home page
 
@@ -10,6 +11,7 @@ describe("OpenOceanStudio:Crew Applications ", () => {
 
     cy.get("#name").click().type("lloyd");
     cy.get('[type="submit"]').click();
+    
   });
 
   it("search and clear the user", () => {
@@ -19,20 +21,53 @@ describe("OpenOceanStudio:Crew Applications ", () => {
 
     cy.get("#name").click().type("lloyd");
     cy.get('[type="submit"]').click();
+    cy.get(".CrewMemeber-name > :nth-child(1)").contains("lloyd");
+    
     // clear the entry and assert for empty name and city fields
 
     cy.get('#filters > [type="button"]').click();
-
     cy.get("#city").should("have.value", "");
     cy.get("#name").clear();
     cy.get("#name").should("have.value", "");
+    
+    // search for the candidate with a specific city
+    
+    cy.get("#city").click().type("hereford");
+    cy.get('[type="submit"]').click();
+    cy.get(":nth-child(1) > :nth-child(1) > :nth-child(2) > .CrewMember-info > .CrewMemeber-name > :nth-child(2)").contains("hereford");
+    
   });
-    
-    
-    it('add candidates to the system', () =>
-    {
 
-        cy.visit("/");
-        
-    })
+  it("add candidates to the system", () => {
+    
+    cy.visit("/");
+    
+    // add candidate to the system
+    
+    cy.get("#name").click().type("adam");
+    cy.get("#city").click().type("newyork");
+    cy.get('[type="submit"]').click();
+    cy.get("#name").clear();
+    cy.get("#city").clear();
+
+    cy.get("#name").should("have.value", "");
+    cy.get("#city").should("have.value", "");
+    
+  });
+
+  it("change the status of candidates", () => {
+    
+    cy.visit("/");
+    
+    cy.get(":nth-child(2) > .CrewMember-toolbar > .CrewMember-up").click();
+    // interviewing
+    
+    cy.get( ":nth-child(2) > :nth-child(1) > .CrewMember-container > .CrewMember-toolbar > .CrewMember-up").click();
+    // hired
+    
+    cy.get(":nth-child(3) > :nth-child(1) > :nth-child(3) > .CrewMember-toolbar > button").click();
+    // applied 
+    
+    cy.get( ":nth-child(2) > :nth-child(1) > .CrewMember-container > .CrewMember-toolbar > :nth-child(1)").click();
+  });
 });
